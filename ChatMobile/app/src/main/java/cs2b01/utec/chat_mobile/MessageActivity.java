cc3b374510162b9fdfc4244b4b9f9d5d24b43647
ErickJR <jordan.rosado@utec.edu.pe>
2019-11-26 10:40:40 -0500

    Congratulations

--- ChatMobile/app/src/main/java/cs2b01/utec/chat_mobile/MessageActivity.java
+++ ChatMobile/app/src/main/java/cs2b01/utec/chat_mobile/MessageActivity.java
@@ -1,8 +1,10 @@
 package cs2b01.utec.chat_mobile;
 
-import android.app.VoiceInteractor;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -15,22 +17,30 @@ import org.json.JSONArray;
 
 public class MessageActivity extends AppCompatActivity {
 
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
         String username = getIntent().getExtras().getString("username");
         setTitle("@"+username);
+        mRecyclerView = findViewById(R.id.main_recycler_view);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        getMessage();
-
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        getMessages();
     }
 
-    public void getMessage(){
+    public Activity getActivity(){
+        return this;
+    }
+
+    public void getMessages(){
         final int userFromId = getIntent().getExtras().getInt("user_from_id");
         final int userToId = getIntent().getExtras().getInt("user_to_id");
 
@@ -39,20 +49,26 @@ public class MessageActivity extends AppCompatActivity {
                 Request.Method.GET,
                 uri,
                 null,
-                new Response.Listener<JSONArray>() {
+                new Response.Listener<JSONArray>(){
                     @Override
                     public void onResponse(JSONArray response) {
-
+                        //TODO process response
+                        mAdapter = new MessageAdapter(response, getActivity(), userFromId);
+                        mRecyclerView.setAdapter(mAdapter);
                     }
                 },
-                new Response.ErrorListener() {
+                new Response.ErrorListener(){
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         error.printStackTrace();
                     }
                 }
         );
+
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(request);
+
+
     }
+
 }
